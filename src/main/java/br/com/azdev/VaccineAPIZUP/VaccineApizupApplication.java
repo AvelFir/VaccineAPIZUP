0b06cfa33141c1928ffdc89bfa5736d5ab8851b8
AvelFir <guichasemartinsgmr@hotmail.com>
2021-03-01 18:42:25 -0300

    first commit

---
+++ src/main/java/br/com/azdev/VaccineAPIZUP/VaccineApizupApplication.java
@@ -0,0 +1,13 @@
+package br.com.azdev.VaccineAPIZUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineApizupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineApizupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/azdev/VaccineAPIZUP/VaccineApizupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azdev.VaccineAPIZUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineApizupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
