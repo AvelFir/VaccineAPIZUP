d4b2ad4a3b81c566261a8758ae0ae851632906f1
AvelFir <guichasemartinsgmr@hotmail.com>
2021-03-01 22:44:57 -0300

    added user entity

---
+++ src/main/java/br/com/azdev/VaccineAPIZUP/entity/Usuario.java
@@ -0,0 +1,64 @@
+package br.com.azdev.VaccineAPIZUP.entity;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nome;
+    @Email @NotNull @Column(unique = true)
+    private String email;
+    @CPF @NotNull @Column(unique = true)
+    private String cpf;
+    @NotNull
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
+
